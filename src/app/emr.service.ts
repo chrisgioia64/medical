cd48fd1b694c642cb1ca9d8a82b5fb1ed9fa4621
Christopher Gioia <christophergioia64@gmail.com>
2024-05-30 14:51:18 -0700

    remove api key

--- src/app/emr.service.ts
+++ src/app/emr.service.ts
@@ -12,7 +12,7 @@ export class EmrService {
   private promptText = "Convert the following text to json format with fields for Name, Age, Gender, "
     + "Causes, Symptoms, Diagnostics, Treatment, Preventative Measures, and Next Steps"
 
-  private apiKey = "sk-proj-sQyQBEEVignDuCqM9VwsT3BlbkFJnsvZV0FRLEs3IE7LNLxm";
+  private apiKey = "";
 
   private apiUrl = 'https://api.openai.com/v1/chat/completions';
 
